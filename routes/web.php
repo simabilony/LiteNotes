cf748fa1f7fb3ab4036bdbaa780d7a5b6793286a
Sima Bilony <www.simabilony@gmail.com>
2024-03-07 00:37:10 +0300

    edit blade

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+ Route::resource('/notes',\App\Http\Controllers\NoteController::class )->middleware(['auth']);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
